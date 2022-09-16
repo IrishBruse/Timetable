8651b27ee5b855fbee82a6e90c20dc71c4fc6ce0
IrishBruse <Econn50@outlook.com>
2022-09-16 16:27:31 +0100

    Update to revised timetable

--- main.js
+++ main.js
@@ -1,51 +1,123 @@
 // https://timetable.gmit.ie/SWS2223/(S(muzwlv55l2uzryvm4ryohl55))/showtimetable.aspx
-const timetableListViewText = `Monday
-Activity	Type	Start	End	Duration	Weeks	Room	Staff
-KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr C/P	P	9:00	10:00	1:00	4-9, 11-17	0436 CR5	Gerard Harrison
-KSOFG3 OPERATING SYSTEMS Gr C/T	T	11:00	12:00	1:00	4-9, 11-17	0156	Dominic Carr
-KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr A/P	P	11:00	12:00	1:00	4-9, 11-17	0484 CR1	Gerard Harrison
-KSOFG3 GRAPHICS PROGRAMMING/L	L	13:00	14:00	1:00	4-9, 11-17	0996	Brian Mc Ginley
-KSOFG3 OPERATING SYSTEMS/L	L	14:00	15:00	1:00	4-9, 11-17	0996	Martin Hynes
+const groupsTimetableText = [
+    `
+Monday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr A/P,P,12:00,13:00,1:00,"4-9, 11-17",0484 CR1,Gerard Harrison
+KSOFG3 GRAPHICS PROGRAMMING/L,L,13:00,14:00,1:00,"4-9, 11-17",996,Brian Mc Ginley
+KSOFG3 OPERATING SYSTEMS/L,L,14:00,15:00,1:00,"4-9, 11-17",996,Martin Hynes
 
 Tuesday
-Activity	Type	Start	End	Duration	Weeks	Room	Staff
-KSOFG3 OPERATING SYSTEMS 1 Gr C/P	P	9:00	11:00	2:00	4-9, 11-17	0436 CR5	Dominic Carr
-KSOFG3 GRAPHICS PROGRAMMING Gr A/P	P	9:00	11:00	2:00	4-9, 11-17	0484 CR1	Brian Mc Ginley
-KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr B/P	P	9:00	11:00	2:00	4-9, 11-17	0480 CR7	Gerard Harrison
-KSOFG3 GRAPHICS PROGRAMMING Gr C/P	P	11:00	13:00	2:00	4-9, 11-17	0480 CR7	Brian Mc Ginley
-KSOFG3 DATA REPRESENTATION AND QUERYING Gr A/P	P	11:00	13:00	2:00	4-9, 11-17	0479 CR8	Martin Kenirons
-KSOFG3 OPERATING SYSTEMS 1 Gr B/P	P	11:00	13:00	2:00	4-9, 11-17	0484 CR1	Martin Hynes
-KSOFG3 DATA REPRESENTATION AND QUERYING Gr B/P	P	14:00	16:00	2:00	4-9, 11-17	0304 CR6	Tbc Sci
-KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L	L	16:00	17:00	1:00	4-9, 11-17	0941	Dominic Carr
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 GRAPHICS PROGRAMMING Gr A/P,P,9:00,11:00,2:00,"4-9, 11-17",0484 CR1,Brian Mc Ginley
+KSOFG3 DATA REPRESENTATION AND QUERYING Gr A/P,P,11:00,13:00,2:00,"4-9, 11-17",0484 CR1,Martin Kenirons
+KSOFG3 GRAPHICS PROGRAMMING/L,L,15:00,16:00,1:00,"4-9, 11-17",941,Brian Mc Ginley
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,16:00,17:00,1:00,"4-9, 11-17",941,Dominic Carr
 
 Wednesday
-Activity	Type	Start	End	Duration	Weeks	Room	Staff
-KSOFG3 OPERATING SYSTEMS Gr B/T	T	9:00	10:00	1:00	4-9, 11-17	PF04	Michael Duignan
-KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr C/P	P	9:00	11:00	2:00	4-9, 11-17	0484 CR1	Gerard Harrison
-KSOFG3 OPERATING SYSTEMS Gr A/T	T	11:00	12:00	1:00	4-9, 11-17	0351	Dominic Carr
-KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr B/P	P	11:00	12:00	1:00	4-9, 11-17	0436 CR5	Gerard Harrison
-KSOFG3 DATA CENTRIC WEB APPLICATIONS/L	L	12:00	13:00	1:00	4-9, 11-17	0995	Gerard Harrison
-KSOFG3 OBJECT ORIENTED PROGRAMMING/L	L	14:00	16:00	2:00	4-9, 11-17	0903	John Healy
-KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L	L	16:00	17:00	1:00	4-9, 11-17	0995	Dominic Carr
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 OPERATING SYSTEMS Gr A/T,T,11:00,12:00,1:00,"4-9, 11-17",368,Dominic Carr
+KSOFG3 DATA CENTRIC WEB APPLICATIONS/L,L,12:00,13:00,1:00,"4-9, 11-17",995,Gerard Harrison
+KSOFG3 OBJECT ORIENTED PROGRAMMING/L,L,14:00,16:00,2:00,"4-9, 11-17",903,John Healy
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,16:00,17:00,1:00,"4-9, 11-17",995,Dominic Carr
 
 Thursday
-Activity	Type	Start	End	Duration	Weeks	Room	Staff
-KSOFG3 OPERATING SYSTEMS/L	L	10:00	11:00	1:00	4-9, 11-17	0996	Martin Hynes
-KSOFG3 OBJECT ORIENTED PROGRAMMING/L	L	12:00	13:00	1:00	4-9, 11-17	0996	John Healy
-KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L	L	14:00	15:00	1:00	4-9, 11-17	0903	Dominic Carr
-KSOFG3/KCDMG4 DATA REPRESENTATION AND QUERYING/L	L	15:00	16:00	1:00	4-9, 11-17	0941	Martin Kenirons
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 OPERATING SYSTEMS/L,L,10:00,11:00,1:00,"4-9, 11-17",996,Martin Hynes
+KSOFG3 OBJECT ORIENTED PROGRAMMING/L,L,12:00,13:00,1:00,"4-9, 11-17",996,John Healy
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,14:00,15:00,1:00,"4-9, 11-17",903,Dominic Carr
+KSOFG3/KCDMG4 DATA REPRESENTATION AND QUERYING/L,L,15:00,16:00,1:00,"4-9, 11-17",941,Martin Kenirons
 
 Friday
-Activity	Type	Start	End	Duration	Weeks	Room	Staff
-KSOFG3 DATA REPRESENTATION AND QUERYING Gr C/P	P	9:00	11:00	2:00	4-9, 11-17	0370 Science Computer Lab	Martin Kenirons
-KSOFG3 GRAPHICS PROGRAMMING Gr B/P	P	9:00	11:00	2:00	4-9, 11-17	0483 CR2	Brian Mc Ginley
-KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr A/P	P	9:00	11:00	2:00	4-9, 11-17	0484 CR1	Gerard Harrison
-KSOFG3 OPERATING SYSTEMS Gr A/P	P	11:00	13:00	2:00	4-9, 11-17	0483 CR2	Martin Hynes
-`;
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr A/P,P,9:00,11:00,2:00,"4-9, 11-17",0484 CR1,Gerard Harrison
+KSOFG3 OPERATING SYSTEMS Gr A/P,P,11:00,13:00,2:00,"4-9, 11-17",0483 CR2,Martin Hynes
+
+`, `
+Monday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 GRAPHICS PROGRAMMING/L,L,13:00,14:00,1:00,"4-9, 11-17",996,Brian Mc Ginley
+KSOFG3 OPERATING SYSTEMS/L,L,14:00,15:00,1:00,"4-9, 11-17",996,Martin Hynes
+
+Tuesday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr B/P,P,9:00,11:00,2:00,"4-9, 11-17",0480 CR7,Gerard Harrison
+KSOFG3 OPERATING SYSTEMS 1 Gr B/P,P,11:00,13:00,2:00,"4-9, 11-17",0479 CR8,Martin Hynes
+KSOFG3 GRAPHICS PROGRAMMING/L,L,15:00,16:00,1:00,"4-9, 11-17",941,Brian Mc Ginley
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,16:00,17:00,1:00,"4-9, 11-17",941,Dominic Carr
+
+Wednesday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 OPERATING SYSTEMS Gr B/T,T,9:00,10:00,1:00,"4-9, 11-17",PF04,Dominic Carr
+KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr B/P,P,11:00,12:00,1:00,"4-9, 11-17",0436 CR5,Gerard Harrison
+KSOFG3 DATA CENTRIC WEB APPLICATIONS/L,L,12:00,13:00,1:00,"4-9, 11-17",995,Gerard Harrison
+KSOFG3 OBJECT ORIENTED PROGRAMMING/L,L,14:00,16:00,2:00,"4-9, 11-17",903,John Healy
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,16:00,17:00,1:00,"4-9, 11-17",995,Dominic Carr
+
+Thursday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 OPERATING SYSTEMS/L,L,10:00,11:00,1:00,"4-9, 11-17",996,Martin Hynes
+KSOFG3 OBJECT ORIENTED PROGRAMMING/L,L,12:00,13:00,1:00,"4-9, 11-17",996,John Healy
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,14:00,15:00,1:00,"4-9, 11-17",903,Dominic Carr
+KSOFG3/KCDMG4 DATA REPRESENTATION AND QUERYING/L,L,15:00,16:00,1:00,"4-9, 11-17",941,Martin Kenirons
+
+Friday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 GRAPHICS PROGRAMMING Gr B/P,P,9:00,11:00,2:00,"4-9, 11-17",0483 CR2,Brian Mc Ginley
+KSOFG3 DATA REPRESENTATION AND QUERYING Gr B/P,P,11:00,13:00,2:00,"4-9, 11-17",0480 CR7,Martin Kenirons
+
+`, `
+Monday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr C/P,P,10:00,11:00,1:00,"4-9, 11-17",0304 CR6,Gerard Harrison
+KSOFG3 OPERATING SYSTEMS Gr C/T,T,11:00,12:00,1:00,"4-9, 11-17",156,Dominic Carr
+KSOFG3 GRAPHICS PROGRAMMING/L,L,13:00,14:00,1:00,"4-9, 11-17",996,Brian Mc Ginley
+KSOFG3 OPERATING SYSTEMS/L,L,14:00,15:00,1:00,"4-9, 11-17",996,Martin Hynes
+
+Tuesday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 OPERATING SYSTEMS 1 Gr C/P,P,9:00,11:00,2:00,"4-9, 11-17",0436 CR5,Dominic Carr
+KSOFG3 GRAPHICS PROGRAMMING Gr C/P,P,11:00,13:00,2:00,"4-9, 11-17",0480 CR7,Brian Mc Ginley
+KSOFG3 GRAPHICS PROGRAMMING/L,L,15:00,16:00,1:00,"4-9, 11-17",941,Brian Mc Ginley
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,16:00,17:00,1:00,"4-9, 11-17",941,Dominic Carr
+
+Wednesday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 DATA CENTRIC WEB APPLICATIONS Gr C/P,P,9:00,11:00,2:00,"4-9, 11-17",0436 CR5,Gerard Harrison
+KSOFG3 DATA CENTRIC WEB APPLICATIONS/L,L,12:00,13:00,1:00,"4-9, 11-17",995,Gerard Harrison
+KSOFG3 OBJECT ORIENTED PROGRAMMING/L,L,14:00,16:00,2:00,"4-9, 11-17",903,John Healy
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,16:00,17:00,1:00,"4-9, 11-17",995,Dominic Carr
+
+Thursday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 OPERATING SYSTEMS/L,L,10:00,11:00,1:00,"4-9, 11-17",996,Martin Hynes
+KSOFG3 OBJECT ORIENTED PROGRAMMING/L,L,12:00,13:00,1:00,"4-9, 11-17",996,John Healy
+KCDMG3/KSOFG3 SOFTWARE QUALITY MANAGEMENT/L,L,14:00,15:00,1:00,"4-9, 11-17",903,Dominic Carr
+KSOFG3/KCDMG4 DATA REPRESENTATION AND QUERYING/L,L,15:00,16:00,1:00,"4-9, 11-17",941,Martin Kenirons
+
+Friday
+
+Activity,Type,Start,End,Duration,Weeks,Room,Staff
+KSOFG3 DATA REPRESENTATION AND QUERYING Gr C/P,P,9:00,11:00,2:00,"4-9, 11-17",0370 Science Computer Lab,Martin Kenirons
+`
+];
 
 const PrintMode = false;
 const Use24Hour = false;
-const groups = ["A", "B", "C"]
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
 const startTime = 9;
@@ -67,7 +139,7 @@ function toModule(name)
     }
     else if (name.includes("DATA REPRESENTATION AND QUERYING"))
     {
-        return { name: "Data Representation And Querying", color: "blue" };
+        return { name: "Data Rep And Querying", color: "blue" };
     }
     else if (name.includes("SOFTWARE QUALITY MANAGEMENT"))
     {
@@ -89,7 +161,6 @@ function RenderGroup(SelectedGroup)
     var body = document.getElementById("root");
     var groupTimetable = document.createElement("div");
     groupTimetable.classList.add("Group");
-    groupTimetable.classList.add(groups[SelectedGroup]);
 
     body.appendChild(groupTimetable);
 
@@ -115,7 +186,7 @@ function RenderGroup(SelectedGroup)
 
     groupTimetable.appendChild(hours);
 
-    var lines = timetableListViewText.split('\n');
+    var lines = groupsTimetableText[SelectedGroup].split('\n');
     var daysModules;
     for (let i = 0; i < lines.length; i++)
     {
@@ -140,17 +211,8 @@ function RenderGroup(SelectedGroup)
             groupTimetable.appendChild(daysModules);
             continue;
         }
-        var data = line.split('\t');
-
-        var group = toGroup(data[0]);
-
-        if (group != "All")
-        {
-            if (group.includes(groups[SelectedGroup]) == false)
-            {
-                continue;
-            }
-        }
+        var data = line.split(',');
+        console.log(data);
 
         var header = document.createElement("div");
         header.className = "moduleTop";
@@ -170,7 +232,7 @@ function RenderGroup(SelectedGroup)
 
         // Room
         element = document.createElement("h5");
-        element.textContent = data[6];
+        element.textContent = data[7];
         element.className = "text"
         element.classList.add("flex-grow")
         header.appendChild(element);
@@ -188,7 +250,7 @@ function RenderGroup(SelectedGroup)
 
         // Lecturer
         element = document.createElement("h5");
-        element.textContent = data[7];
+        element.textContent = data[8];
         element.className = "text"
         footer.appendChild(element);
 
@@ -202,7 +264,8 @@ function RenderGroup(SelectedGroup)
         if (PrintMode)
         {
             module.classList.add("print");
-        } else
+        }
+        else
         {
             module.classList.add(mod.color);
         }
@@ -250,49 +313,32 @@ function toType(type)
 {
     switch (type[0])
     {
-        case "O": return "(Online)"
         case "P": return "(Practical)"
         case "L": return "(Lecture)"
-        default: return "ERROR: " + type;
+        case "T": return "(Tutorial)"
+        case "S": return "(Seminar)"
+        case "OL": return "(Online)"
+        case "Lab": return "(Lab)"
+        default: return "(" + type + ")";
     }
-
 }
 
-for (let index = 0; index < groups.length; index++)
+let i = 0;
+
+Test(i);
+
+document.body.onkeyup = function (e)
 {
-    const g = groups[index];
-
-    RenderGroup(index)
-
-    const button = document.createElement('button');
-    button.className = "download";
-
-    const a = document.createElement('a');
-    a.className = "Group" + g + "Download"
-    a.innerText = "Download Group " + g
-    a.setAttribute('download', "Group " + g + '.png');
-
-
-    button.appendChild(a);
-
-    document.querySelector("#buttons").appendChild(button)
-
-    setTimeout(() =>
+    if (e.keyCode == 32)
     {
-        html2canvas(document.querySelector(".Group." + g))
-            .then(canvas =>
-            {
-                canvas.style.display = 'none';
-                document.body.appendChild(canvas);
+        i++
+        Test(i)
+    }
+}
 
-                return canvas;
-            })
-            .then(canvas =>
-            {
-                const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
-                document.querySelector(".Group" + g + "Download").setAttribute('href', image);
-                document.querySelector(".Group" + g + "Download").parentElement.style.backgroundColor = "#67e672";
-
-            });
-    }, 1);
+function Test(i)
+{
+    document.querySelector("#Title").textContent = "Group " + String.fromCharCode(((i % groupsTimetableText.length) + 97)).toUpperCase()
+    document.querySelector("#root").innerHTML = ""
+    RenderGroup(i % groupsTimetableText.length)
 }
